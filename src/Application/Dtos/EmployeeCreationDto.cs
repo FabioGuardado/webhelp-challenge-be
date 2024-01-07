c15eea12d3759c1c430aa6963e85b35eb4251b74
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-01-07 12:53:26 -0600

    Add CreateEmployee service

---
+++ src/Application/Dtos/EmployeeCreationDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebhelpChallengeBackend.Domain.Entities;
+
+namespace WebhelpChallengeBackend.Application.Dtos;
+public class EmployeeCreationDto
+{
+    public int Id { get; set; }
+    public string? Nombres { get; set; }
+    public string? Apellidos { get; set; }
+    public int TipoDeDocumentoId { get; set; }
+    public string? NumeroDeDocumento { get; set; }
+    public DateOnly FechaDeContratacion { get; set; }
+    public int PaisId { get; set; }
+    public int AreaId { get; set; }
+    public int SubAreaId { get; set; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Empleado, EmployeeCreationDto>();
+            CreateMap<EmployeeCreationDto, Empleado>();
+        }
+    }
+}
--- src/Web/Endpoints/Employees.cs
+++ src/Web/Endpoints/Employees.cs
@@ -1,6 +1,7 @@
 ﻿
 using WebhelpChallengeBackend.Application.Common.Models;
 using WebhelpChallengeBackend.Application.Dtos;
+using WebhelpChallengeBackend.Application.Employees.Commands.CreateEmployee;
 using WebhelpChallengeBackend.Application.Employees.Queries.GetAllEmployees;
 
 namespace WebhelpChallengeBackend.Web.Endpoints;
@@ -10,11 +11,19 @@ public class Employees : EndpointGroupBase
     public override void Map(WebApplication app)
     {
         app.MapGroup(this)
-            .MapGet(GetAllEmployees);
+            .MapGet(GetAllEmployees)
+            .MapPost(CreateEmployee);
     }
 
     public async Task<PaginatedList<EmployeeDto>> GetAllEmployees(ISender sender, [AsParameters] GetAllEmployeesQuery query)
     {
         return await sender.Send(query);
     }
+
+    public async Task<IResult> CreateEmployee(ISender sender, CreateEmployeeCommand command)
+    {
+        await sender.Send(command);
+        return Results.Created();
+    }
+
 }
